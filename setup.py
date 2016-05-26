474f8d7ec2b93bc077e0de48e2f3c492c6d6d538
zeryx <james@algorithmia.com>
2016-05-26 14:20:00 -0700

    adjusted tensorflow format to be the same that pypi has.

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         'numpy>=0.7',
 	'protobuf==3.0.0b2',
 	'asciitree',
-	'https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl',
+	'tensorflow >=0.7.0',
     ],
 )
